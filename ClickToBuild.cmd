427b49f35980325c5ec1e83812b9215c2c1449db
vmadykumar <30862337+vmadykumar@users.noreply.github.com>
2020-03-29 19:09:59 +0530

    Update ClickToBuild.cmd

--- ClickToBuild.cmd
+++ ClickToBuild.cmd
@@ -2,6 +2,6 @@
 cls
 echo Building SmartStore.NET...   											
 
-call build.bat /t:Deploy
+call build.bat /p:DeployOnBuild=true /p:DeployDefaultTarget=WebPublish /p:WebPublishMethod=FileSystem /p:SkipInvalidConfigurations=true /t:build /p:Configuration=Release /p:Platform=\"Any CPU\" /p:DeleteExistingFiles=True
 
 pause
